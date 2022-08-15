e572d9cefd36bc4444126a5d5bb1ed19954e1eed
Loai Nazeer <loaiwalid8@gmail.com>
2022-08-15 05:31:34 +0200

    version 2.00

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def set_bg_hack_url():
          f"""
          <style>
          .stApp {{
-             background: url("https://images.pexels.com/photos/399187/pexels-photo-399187.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+             background: url("https://img.freepik.com/premium-photo/sport-stadium-background-flashing-lights-glowing-stadium-lights_327072-1327.jpg");
              background-size: cover    
          }}
          </style>
@@ -71,25 +71,67 @@ def set_bg_hack_url():
          unsafe_allow_html=True
      )
 
-#set_bg_hack_url()
+set_bg_hack_url()
 
-st.title('Fantasy in Your Pocket')
-st.image('Picture1.png')
+st.title('Fantasy in Your Pocket',)
+st.image('http://www.uptheclarets.com/wp-content/uploads/2019/06/fantasy-premier-league-2019-20-banner.jpg',use_column_width=True)
+#st.image('Picture1.png')
 
 #Get data from football overload
 data = pd.read_csv('http://fantasyoverlord.com/FPL/DataWithForecasts',encoding='iso8859_2')
+cols = ['FirstName', 'Surname', 'PositionsList', 'Team', 'Cost','PointsLastRound']
 
 #Top players untile this GW [Total]
+cols.append('TotalPoints')
 st.header('Top Players in all the fantasy')
-st.write(data.sort_values(by=['TotalPoints'],ascending=False,ignore_index=True)[data.columns[:(data.columns.get_indexer(['NextFixture5'])+2)[0]]].head(50))
+st.write(data.sort_values(by=['TotalPoints'],ascending=False,ignore_index=True)[cols].head(50))
+cols.remove('TotalPoints')
 
 #Top palyer in the last GW
 st.header('Top Players in the Last Weak')
-st.write(data.sort_values(by=['PointsLastRound'],ascending=False,ignore_index=True)[data.columns[:(data.columns.get_indexer(['NextFixture5'])+2)[0]]].head(50))
+st.write(data.sort_values(by=['PointsLastRound'],ascending=False,ignore_index=True)[cols].head(50))
 
 #Top selected Plyers
+cols.append('SelectedByPercent')
 st.header('Top Players Selected by Users')
-st.write(data.sort_values(by=['SelectedByPercent'],ascending=False,ignore_index=True)[data.columns[:(data.columns.get_indexer(['NextFixture5'])+2)[0]]].head(50))
+st.write(data.sort_values(by=['SelectedByPercent'],ascending=False,ignore_index=True)[cols].head(50))
+cols.remove('SelectedByPercent')
+
+#Top Transfers Out this round
+cols.append('TransfersOutRound')
+st.header('Top Transfers Out in this round')
+st.write(data.sort_values(by=['TransfersOutRound'],ascending=False,ignore_index=True)[cols].head(50))
+cols.remove('TransfersOutRound')
+
+#Top Transfers In this round
+cols.append('TransfersInRound')
+st.header('Top Transfers In in this round')
+st.write(data.sort_values(by=['TransfersInRound'],ascending=False,ignore_index=True)[cols].head(50))
+cols.remove('TransfersInRound')
+
+#How much a player’s price has increased since the start of the season
+cols.append('PriceRise')
+st.header('How much a player’s price has increased since the start of the season')
+st.write(data.sort_values(by=['PriceRise'],ascending=False,ignore_index=True)[cols].head(50))
+cols.remove('PriceRise')
+
+# How much a player’s price has increased this Gameweek.
+cols.append('PriceRiseRound')
+st.header('How much a player’s price has increased this Gameweek.')
+st.write(data.sort_values(by=['PriceRiseRound'],ascending=False,ignore_index=True)[cols].head(50))
+cols.remove('PriceRiseRound')
+
+#How much a player’s price has decreased since the start of the season
+cols.append('PriceFall')
+st.header('How much a player’s price has decreased since the start of the season')
+st.write(data.sort_values(by=['PriceFall'],ascending=False,ignore_index=True)[cols].head(50))
+cols.remove('PriceFall')
+
+# How much a player’s price has decreased this Gameweek.
+cols.append('PriceFallRound')
+st.header('How much a player’s price has decreased this Gameweek')
+st.write(data.sort_values(by=['PriceFallRound'],ascending=False,ignore_index=True)[cols].head(50))
+cols.remove('PriceFallRound')
 
 # Forcasting
 st.header('Forcasting The Top Players for the Next Week ')
